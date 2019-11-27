9c0b2502c1280a85ed2d65a037282fe2908b2440
Jolyon Wharton <jolyon.wharton@webenabled.tv>
2019-11-27 20:37:22 +0000

    beginings of metadata stripping from wav file, none functional

--- We.Sparkie.DigitalAsset.Api/Entities/Asset.cs
+++ We.Sparkie.DigitalAsset.Api/Entities/Asset.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace We.Sparkie.DigitalAsset.Api.Entities
 {
     public class Asset : Entity
     {
+        private Dictionary<string, IEncodingStrategy> _encodingStrategies;
+
+        public Asset()
+        {
+            _encodingStrategies = new Dictionary<string, IEncodingStrategy>
+            {
+                { "wav", new WavStrategy() }
+            };
+        }
+
         public string Name { get; set; }
         public decimal Size { get; set; }
         public int BitDepth { get; set; }
-        public int SampleRate { get; set; }
+        public decimal SampleRate { get; set; }
         public Uri Location { get; set; }
+        public string Genre { get; set; }
+
+
+        public void Upload(string name, MemoryStream audioStream)
+        {
+            Name = name;
+            var typeFromExtension = ExtractTypeFromExtension(name);
+            _encodingStrategies[typeFromExtension].PopulateAssetMetadata(this, audioStream);
+        }
+
+        private string ExtractTypeFromExtension(string name)
+        {
+            return "wav";
+        }
     }
 }
