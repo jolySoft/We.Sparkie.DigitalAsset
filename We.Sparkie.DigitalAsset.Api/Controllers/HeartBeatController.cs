51173f5a6f98b3abd0995cd2c0019afc62575e89
jolySoft <jolyon.wharton@webenabled.tv>
2021-05-30 11:40:21 +0100

    upgeaded docker image to dotnet 5, heartbeat added

---
+++ We.Sparkie.DigitalAsset.Api/Controllers/HeartBeatController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace We.Sparkie.DigitalAsset.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeartBeatController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
